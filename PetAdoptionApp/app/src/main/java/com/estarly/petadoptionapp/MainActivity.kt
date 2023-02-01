0b95ee405ddfd26a26042d4aae058e99a609b128
Estarly Amaya <estarly.amaya@servinformacion.com>
2023-02-01 10:21:31 -0500

    CREATION
    - Start proyect

---
+++ PetAdoptionApp/app/src/main/java/com/estarly/petadoptionapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.estarly.petadoptionapp
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.estarly.petadoptionapp.ui.theme.PetAdoptionAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Pet-Adoption-App
+
+This proyect is an app to practice  Android Jetpack compose,advanced Retrofit, Rxjava and other android concepts
